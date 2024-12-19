3de584f511fb43b62a620e66507bc2dbab6891d5
TruongHoang03 <hoangthor2003@gmail.com>
2024-12-19 16:13:52 +0700

    google, github
    đăng nhập bằng google và github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,22 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from '@/lib/prismadb';
 import { compare} from 'bcrypt'
+
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { threadId } from "worker_threads";
 export default NextAuth({
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
@@ -44,7 +57,8 @@ export default NextAuth({
     pages: {
         signIn: '/auth'
     },
-    debug: process.env.NODE_ENV == 'development',
+    debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt'
     },
